fc28fe45314f47d487ebe583f6caf63625e57df6
Andrey <65460291+eXeruvim@users.noreply.github.com>
2022-11-30 02:58:30 +0300

    Processed the main page of the orders (first page)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.IOException;
@@ -12,23 +14,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         List<Product> productList = new ArrayList<>();
         String link =
-                "https://ru.banggood.com/Wholesale-Computers-and-Office-ca-5001.html?bid=210707&from=nav";
+                "https://ru.banggood.com/ru/Wholesale-Computers-and-Office-ca-5001.html?bid=210707&from=nav&a=1669744527.8678&DCC=RU&currency=RUB";
         System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
         webDriver.get(link);
-        Thread.sleep(1000*2);
+        Thread.sleep(4000);
         Document document = Jsoup.parse(webDriver.getPageSource());
-        Elements elements = document.getElementsByClass("goodlist");
-        //elements.forEach(System.out::println);
+        Elements elements = document.getElementsByClass("product-list");
         for (Element element : elements) {
-            Product product = new Product();
             Document doc = Jsoup.parse(element.html());
-            product.setTitle(doc.getElementsByClass("title").first().text());
-            productList.add(product);
+
+            Element listElement = doc.getElementsByClass("goodlist").first();
+
+            Document ordersDocument = Jsoup.parse(listElement.html());
+            Elements ordersElements = ordersDocument.getElementsByClass("p-wrap exclick");
+            System.out.println(ordersElements.size());
+            for (Element ordersElement : ordersElements) {
+                Product product = new Product();
+                product.setUrl(ordersElement.child(0).getElementsByClass("img notranslate").select("a").attr("href"));
+                product.setSalePriceRUB(ordersElement.child(1).getElementsByClass("price").first().text());
+                product.setRub(ordersElement.child(2).getElementsByClass("price-old").first().text());
+                product.setTitle(ordersElement.child(3).getElementsByClass("title").select("a").attr("title"));
+
+
+
+                Thread.sleep(1000);
+                productList.add(product);
+            }
+
+/*
+            Elements saleElements = ordersDocument.getElementsByClass("price");
+
+
+            product.setSalePriceUSD(product.getSalePriceRUB());
+
+            Element priceElement = doc.getElementsByClass("price-old").first();
+            product.setRub(priceElement.text());
+            product.setUsd(product.getRub());
+
+            Document order = Jsoup.connect(titleElement.attr("href")).get();
+
+            Element ratingElement = order.getElementsByClass("star-num js-star-num").first();
+            product.setRating(ratingElement.text());
+
+            Element imgElement = order.getElementsByClass("image-max").first().child(1);
+            product.setPicture(imgElement.attr("src"));
+
+            Element reviewsElement = order.getElementsByClass("rating-num J-rating-num").first();
+            product.setReviews(reviewsElement.text().replaceAll("[^0-9]", ""));
+*/
+
         }
         productList.forEach(System.out::println);
     }
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -6,6 +6,11 @@ public class Product {
     private String usd;
     private String rub;
     private String picture;
+    private String salePriceRUB;
+    private String salePriceUSD;
+    private String rating;
+    private String reviews;
+
 
     public String getTitle() {
         return title;
@@ -28,7 +33,7 @@ public class Product {
     }
 
     public void setUsd(String usd) {
-        this.usd = usd;
+        this.usd = String.valueOf(Math.round(Float.parseFloat(getRub()) / 61.07));
     }
 
     public String getRub() {
@@ -36,7 +41,7 @@ public class Product {
     }
 
     public void setRub(String rub) {
-        this.rub = rub;
+        this.rub = rub.replaceAll("\\.", "").replaceAll("\\s", "").replaceAll("[^0-9,]", "").replaceAll(",", ".");
     }
 
     public String getPicture() {
@@ -47,14 +52,52 @@ public class Product {
         this.picture = picture;
     }
 
+    public String getsalePriceRUB() {
+        return salePriceRUB;
+    }
+
+    public void setSalePriceRUB(String salePriceRUB) {
+        this.salePriceRUB = salePriceRUB.replaceAll("\\.", "").replaceAll("\\s", "").replaceAll("[^0-9,]", "").replaceAll(",", ".");
+    }
+
+    public String getSalePriceRUB() {
+        return salePriceRUB;
+    }
+
+    public String getSalePriceUSD() {
+        return salePriceUSD;
+    }
+
+    public void setSalePriceUSD(String salePriceUSD) {
+        this.salePriceUSD = String.valueOf(Math.round(Float.parseFloat(getSalePriceRUB()) / 61.07));
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(String reviews) {
+        this.reviews = reviews;
+    }
+
     @Override
     public String toString() {
         return "Product{" +
-                "title='" + title + '\'' +
-                ", url='" + url + '\'' +
-                ", usd='" + usd + '\'' +
-                ", rub='" + rub + '\'' +
-                ", picture='" + picture + '\'' +
+                "Товар = '" + title + '\'' + "\n" +
+                "Ссылка = '" + url + '\'' + "\n" +
+                "Цена = '" + rub + " руб. (" + usd + " $)" + '\'' + "\n" +
+                "Изображение = '" + picture + '\'' + "\n" +
+                "Цена по акции = '" + salePriceRUB + " руб. (" + salePriceUSD + " $)"  + '\'' + "\n" +
+                "Рейтинг = '" + rating + '\'' + "\n" +
+                "Отзывов = '" + reviews + '\'' +
                 '}';
     }
 }
