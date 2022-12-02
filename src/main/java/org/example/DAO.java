751b255397687b3a6b84658d66e1dbd568f72823
Andrey <65460291+eXeruvim@users.noreply.github.com>
2022-12-03 00:09:35 +0300

    fixes

--- src/main/java/org/example/DAO.java
+++ src/main/java/org/example/DAO.java
@@ -1,25 +1,33 @@
 package org.example;
 
-import java.io.IOException;
 import java.sql.*;
 import java.util.List;
 
 public class DAO {
-    public static void insertDB () throws IOException, InterruptedException {
+    public static void insertDB () {
         List<Product> productList = DOM.getDOM();
-
+        int posts = 0;
+        int skip = 0;
         try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/exeruvim", "root", "")) {
             Statement statement = connection.createStatement();
 
             for (Product product : productList){
-                ResultSet getID = statement.executeQuery("SELECT ID+1 FROM wp_exeruvimposts ORDER BY ID DESC LIMIT 1;");
-                getID.next();
-                long id = getID.getLong(1);
-                String html = "'<!-- wp:paragraph -->\n <img src=\"" + product.getPicture() + "\" </img> \n <p> <b> Товар: " + product.getTitle() + "</b> </p> \n <p> <b> " + product.getOption() + " </b>" + product.getVariety() + "</p> \n <p> <b> Цена по акции: </b> " + product.getSalePriceRUB() + " руб. (" + product.getSalePriceUSD() + " $)" + "</p> \n <p> <b> Цена: </b>" + product.getRub() + " руб. (" + product.getUsd() + " $)" +  "</p> \n <p> <b> Рейтинг: </b>" + product.getRating() + "</p> \n <p> <b> Количество отзывов: </b>" + product.getReviews() + "</p> \n <a href=\"" + product.getUrl() + "\"> <b> Купить </b> </a> \n <!-- /wp:paragraph -->'";
-                String sql = "INSERT INTO `wp_exeruvimposts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES ("+id+", 1, NOW(), UTC_TIMESTAMP(), "+ html +", '"+product.getTitle()+"', '', 'publish','open','open', '', 'order"+id+"', '', '', NOW(), UTC_TIMESTAMP(), '', 0, 'http://exeruvim.su/?p="+id+"', 0,'post', '', 0)";
-                statement.executeUpdate(sql);
+                ResultSet getPost = statement.executeQuery("Select count(*) from wp_exeruvimposts where post_title like '" + product.getTitle() + "';");
+                getPost.next();
+                if (getPost.getLong(1) == 0) {
+                    ResultSet getID = statement.executeQuery("SELECT ID+1 FROM wp_exeruvimposts ORDER BY ID DESC LIMIT 1;");
+                    getID.next();
+                    long id = getID.getLong(1);
+                    String html = "'<!-- wp:paragraph -->\n <img src=\"" + product.getPicture() + "\" </img> \n <p> <b> Товар: " + product.getTitle() + "</b> </p> \n <p> <b> " + product.getOption() + " </b>" + product.getVariety() + "</p> \n <p> <b> Цена по акции: </b> " + product.getSalePriceRUB() + " руб. (" + product.getSalePriceUSD() + " $)" + "</p> \n <p> <b> Цена: </b>" + product.getRub() + " руб. (" + product.getUsd() + " $)" + "</p> \n <p> <b> Рейтинг: </b>" + product.getRating() + "</p> \n <p> <b> Количество отзывов: </b>" + product.getReviews() + "</p> \n <a href=\"" + product.getUrl() + "\"> <b> Купить </b> </a> \n <!-- /wp:paragraph -->'";
+                    String sql = "INSERT INTO `wp_exeruvimposts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES (" + id + ", 1, NOW(), UTC_TIMESTAMP(), " + html + ", '" + product.getTitle() + "', '', 'publish','open','open', '', 'order" + id + "', '', '', NOW(), UTC_TIMESTAMP(), '', 0, 'http://exeruvim.su/?p=" + id + "', 0,'post', '', 0)";
+                    statement.executeUpdate(sql);
+                    posts ++;
+                }
+                else skip++;
+
             }
-            System.out.println("Данные успешно добавлены");
+            if (skip > 0) System.out.println("Некоторые товары уже находятся в базе данных. \t товаров пропущено: " + skip);
+            if (posts > 0) System.out.println("Данные успешно добавлены." + "\t товаров загружено: " + posts);
             statement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/main/java/org/example/DOM.java
+++ src/main/java/org/example/DOM.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -7,49 +8,56 @@ import org.jsoup.select.Elements;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
 
 public class DOM {
-    public static List<Product> getDOM() throws InterruptedException, IOException {
+
+    public static @NotNull List<Product> getDOM() {
+
         List<Product> productList = new ArrayList<>();
         String link =
                 "https://ru.banggood.com/ru/Wholesale-Computers-and-Office-ca-5001.html?bid=210707&from=nav&a=1669744527.8678&DCC=RU&currency=RUB";
         System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
-        webDriver.get(link);
-        Thread.sleep(4000);
-        Document document = Jsoup.parse(webDriver.getPageSource());
-        document = Jsoup.parse(document.getElementsByClass("product-list").html());
+        try {
+            webDriver.get(link);
+            Thread.sleep(4000);
 
-        Document ordersDocument = Jsoup.parse(Objects.requireNonNull(document.getElementsByClass("goodlist").first()).html());
-        Elements ordersElements = ordersDocument.getElementsByClass("p-wrap exclick");
-        //System.out.println(ordersElements.size());
-        for (Element ordersElement : ordersElements) {
-            Product product = new Product();
-            // Парсинг со страницы товаров
-            product.setUrl(ordersElement.child(0).getElementsByClass("img notranslate").select("a").attr("href"));
-            product.setSalePriceRUB(Objects.requireNonNull(ordersElement.child(1).getElementsByClass("price").first()).text());
-            product.setRub(Objects.requireNonNull(ordersElement.child(2).getElementsByClass("price-old").first()).text());
-            product.setTitle(ordersElement.child(3).getElementsByClass("title").select("a").attr("title"));
-            product.setReviews(ordersElement.child(4).getElementsByClass("review").select("a").text());
+            Document document = Jsoup.parse(webDriver.getPageSource());
+            document = Jsoup.parse(document.getElementsByClass("product-list").html());
+            Document ordersDocument = Jsoup.parse(Objects.requireNonNull(document.getElementsByClass("goodlist").first()).html());
+            Elements ordersElements = ordersDocument.getElementsByClass("p-wrap exclick");
+            //System.out.println(ordersElements.size());
+            for (Element ordersElement : ordersElements) {
+                Product product = new Product();
+                // Парсинг со страницы товаров
+                product.setUrl(ordersElement.child(0).getElementsByClass("img notranslate").select("a").attr("href"));
+                product.setSalePriceRUB(Objects.requireNonNull(ordersElement.child(1).getElementsByClass("price").first()).text());
+                product.setRub(Objects.requireNonNull(ordersElement.child(2).getElementsByClass("price-old").first()).text());
+                product.setTitle(ordersElement.child(3).getElementsByClass("title").select("a").attr("title"));
+                product.setReviews(ordersElement.child(4).getElementsByClass("review").select("a").text());
 
-            // Парсинг со страницы товара
-            Document order = Jsoup.connect(ordersElement.child(0).getElementsByClass("img notranslate").select("a").attr("href")).get();
+                // Парсинг со страницы товара
+                Document order = Jsoup.connect(ordersElement.child(0).getElementsByClass("img notranslate").select("a").attr("href")).get();
 
-            product.setPicture(order.getElementsByClass("image-max").select("img").attr("src"));
-            product.setRating(order.getElementsByClass("star-num js-star-num").text());
-            product.setOption(Objects.requireNonNull(order.getElementsByClass("block-title").select("span").first()).text());
-            product.setVariety(order.getElementsByClass("block-cnt").select("a").eachAttr("title"));
-            product.setUsd(product.getRub());
-            product.setSalePriceUSD(product.getSalePriceRUB());
+                product.setPicture(order.getElementsByClass("image-max").select("img").attr("src"));
+                product.setRating(order.getElementsByClass("star-num js-star-num").text());
+                product.setOption(Objects.requireNonNull(order.getElementsByClass("block-title").select("span").first()).text());
+                product.setVariety(order.getElementsByClass("block-cnt").select("a").eachAttr("title"));
+                product.setUsd(product.getRub());
+                product.setSalePriceUSD(product.getSalePriceRUB());
 
-            Thread.sleep(1000);
-            productList.add(product);
+                Thread.sleep(1000);
+                productList.add(product);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            webDriver.quit();
         }
         return productList;
     }
